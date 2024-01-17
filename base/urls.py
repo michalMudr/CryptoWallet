dee7a34654dc2d064d4b979ff32de7c067db188e
michalMudr <270074346@yoobeestudent.ac.nz>
2024-01-17 22:19:49 +1300

    Cryptocurrency build version 1

--- base/urls.py
+++ base/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     path('get_chain', views.get_chain, name="get_chain"),
     path('mine_block', views.mine_block, name="mine_block"),
     path('is_valid', views.is_valid, name="is_valid"),
+    path('add_transaction', views.add_transaction, name="add_transaction"), #New
+    path('connect_node', views.connect_node, name="connect_node"), #New
+    path('replace_chain', views.replace_chain, name="replace_chain"), #New
 ]
